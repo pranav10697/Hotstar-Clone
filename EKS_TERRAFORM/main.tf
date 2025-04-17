6e103bf5d70d9558dca872058b318aecb17397bf
pranav10697 <166798910+pranav10697@users.noreply.github.com>
2025-04-17 17:15:13 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud1"
+  name               = "eks-cluster-cloud"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -80,7 +80,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
 
 #create node group
 resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name1
+  cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud1"
   node_role_arn   = aws_iam_role.example1.arn
   subnet_ids      = data.aws_subnets.public.ids
